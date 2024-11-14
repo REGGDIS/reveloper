896ebe541404b3d66f6fb7f7c6ebaa6fe2d2aefc
REGGDIS <regdis@gmail.com>
2024-11-14 18:35:06 -0300

    Encriptación de contraseña

--- Reveloper/admin.py
+++ Reveloper/admin.py
@@ -1,8 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 from .models import Usuario, Proyecto, TareaPorDesarrollar, TareaDesarrollada, Evaluacion
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+
+
+class CustomUserAdmin(BaseUserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2'),
+        }),
+    )
+
 
 # Registro de Modelos
-admin.site.register(Usuario)
+admin.site.register(Usuario, CustomUserAdmin)
 admin.site.register(Proyecto)
 admin.site.register(TareaPorDesarrollar)
 admin.site.register(TareaDesarrollada)
--- Reveloper/forms.py
+++ Reveloper/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from .models import TareaPorDesarrollar
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from .models import TareaPorDesarrollar, Usuario
 
 
 class TareaPorDesarrollarForm(forms.ModelForm):
@@ -7,3 +8,15 @@ class TareaPorDesarrollarForm(forms.ModelForm):
         model = TareaPorDesarrollar
         fields = ['titulo', 'descripcion',
                   'fecha_vencimiento', 'estado', 'proyecto']
+
+
+class CustomUserCreationForm(UserCreationForm):
+    class Meta:
+        model = Usuario
+        fields = ('username', 'email')
+
+
+class CustomUserChangeForm(UserChangeForm):
+    class Meta:
+        model = Usuario
+        fields = '__all__'
